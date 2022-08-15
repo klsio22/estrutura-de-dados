9d2346d4fbf73f1c9a889c774707eb8d18202de8
Klesio Nascimento <klesio.dev@gmail.com>
2022-08-15 01:19:00 -0300

    Test started in the vector

---
+++ src/Test_class_vector/TestVector.java
@@ -0,0 +1,13 @@
+package Test_class_vector;
+
+import Vector_base.Vector;
+
+public class TestVector {
+    public static void main(String[] args) {
+        Vector vector = new Vector(5);
+
+        int i = 0;
+        i++;
+
+    }
+}
---
+++ src/Vector_base/Vector.java
@@ -0,0 +1,9 @@
+package Vector_base;
+
+public class Vector {
+    private String[] elements;
+
+    public Vector(int capacity) {
+        elements = new String[capacity];
+    }
+}
