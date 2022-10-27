c7e3c58c8c18e28eb151cf6a9fa4ec9e0d1a4d1d
Klesio Nascimento <klesio.dev@gmail.com>
2022-10-27 18:30:32 -0300

    Added new tests for work  1 utpf

---
+++ src/testes/Indexof.java
@@ -0,0 +1,20 @@
+package testes;
+
+public class Indexof {
+
+
+    public static void main(String[] args) {
+
+        Search search = new Search();
+
+        String pat = "AABAACAADAABAAABAA";
+        String txt = "AABA";
+
+
+        search.search(pat, txt);
+
+
+    }
+
+
+}
