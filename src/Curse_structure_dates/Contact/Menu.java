53a2c013978fba0340b1755520d1f5a2976063bb
klesio <klesio.dev@gmail.com>
2022-12-04 19:01:43 -0300

    added addContactPosition and addFinalContact

--- src/Curse_structure_dates/Contact/Menu.java
+++ src/Curse_structure_dates/Contact/Menu.java
@@ -6,77 +6,156 @@ import java.util.Scanner;
 
 public class Menu {
 
-    public void changeOption(Scanner scan) {
-        int option = 1;
-        while (option != 0) {
-            option = getOptionMenu(scan);
+  public void changeOption(Scanner scan, Lists<Contact> list) {
+    int option = 1;
+    while (option != 0) {
+      option = getOptionMenu(scan);
 
-            switch (option){
-                case 1:break;
-                case 2:break;
-                default:break;
-            }
+      switch (option) {
+        case 1:
+          addFinalContact(scan, list);
+          break;
+        case 2:
+          addContactPosition(scan,list);
+          break;
+        case 3:
+          break;
+        case 4:
+          break;
+        case 5:
+          break;
+        case 6:
+          break;
+        case 7:
+          break;
+        case 8:
+          break;
+        case 9:
+          break;
+        case 10:
+          break;
+        case 11:
+          break;
+        default:
+          break;
+      }
+    }
+    System.out.println("Usuário digitou 0 , programa terminado");
+  }
 
+  public void addFinalContact(Scanner scan, Lists<Contact> list) {
+    System.out.println("Criado um contato, entre com as informações");
+    String name = readInformation("Entre com o telefone", scan);
+    String phone = readInformation("Entre com o telefone", scan);
+    String email = readInformation("Entre com o telefone", scan);
+
+    Contact contact = new Contact(name, phone, email);
+    list.add(contact);
+    System.out.println("Contato adicionado com sucesso");
+    System.out.println(contact);
+  }
+
+  public void addContactPosition(Scanner scan, Lists<Contact> list) {
+    System.out.println("Criado um contato, entre com as informações");
+    String name = readInformation("Entre com o telefone", scan);
+    String phone = readInformation("Entre com o telefone", scan);
+    String email = readInformation("Entre com o telefone", scan);
+
+    Contact contact = new Contact(name, phone, email);
+
+    int pos = readInformationInt("Entre com a posição a adicionar o contato", scan);
+
+
+    try {
+      list.add(pos, contact);
+
+      System.out.println("Contato adicionado com sucesso");
+      list.add(contact);
+    } catch (Exception e) {
+      System.out.println("Posição inválida, contato não adiconanado\n\n");
+    }
+
+    System.out.println("Contato adicionado com sucesso");
+    System.out.println(contact);
+  }
+
+  public int readInformationInt(String msg, Scanner scan) {
+    boolean inputValid = false;
+    String input;
+    int num = 0;
+    while (!inputValid) {
+      try {
+        System.out.println(msg);
+        input = scan.nextLine();
+        num = Integer.parseInt(input);
+
+        inputValid = true;
+      } catch (Exception e) {
+        System.out.println("Entrada inválida , digite novamente\n\n");
+      }
+    }
+    return num;
+  }
+
+  public String readInformation(String msg, Scanner scan) {
+    String input;
+    System.out.println(msg);
+    input = scan.nextLine();
+
+    return input;
+  }
+
+
+  public int getOptionMenu(Scanner scan) {
+    boolean inputValid = false;
+    int selectOption = 0;
+    String input;
+
+    String option = "Digite a opção desejada:\n" +
+            "1:Adiconar contato no final do vetor:\n" +
+            "2: Adicionar contato em uma posição especifica\n" +
+            "3: Obtém contato de uma posição especírfica\n" +
+            "4: Consulta contato\n" +
+            "5: Consulta útimo índice do contato\n" +
+            "6: Verifica se contato existe\n" +
+            "7: Excluir por posição\n" +
+            "8: Excluir contato\n" +
+            "9: Verifica tamanho do vetor\n" +
+            "10: Excluir todos os contatos do vetor\n" +
+            "11: Impreme vetor\n" +
+            "0: sair";
+
+    while (!inputValid) {
+      System.out.println(option);
+
+      try {
+        input = scan.nextLine();
+        selectOption = Integer.parseInt(input);
+        if (selectOption >= 0 && selectOption <= 11) {
+          inputValid = true;
+        } else {
+          throw new Exception();
         }
-
-        System.out.println("Usuario digitou 0 , programa terminado");
-    }
-
-    public void adcionarContatoCOntatoFinal(){
-
+      } catch (Exception e) {
+        System.out.println("Entrada inválida , digite novamente\n\n");
+      }
     }
 
 
-    public int getOptionMenu(Scanner scan) {
-        boolean inputValid = false;
-        int selectOption = 0;
-        String input;
+    return selectOption;
+  }
 
-        String option = "Digite a opção desejada:\n" +
-                "1:Adiconar contato no final do vetor:\n" +
-                "2: Adicionar contato em uma posição especifica\n" +
-                "3: Obtém contato de uma posição especírfica\n" +
-                "4: Consulta contato\n" +
-                "5: Consulta útimo índice do contato\n" +
-                "6: Verifica se contato existe\n" +
-                "7: Excluir por posição\n" +
-                "8: Excluir contato\n" +
-                "9: Verifica tamanho do vetor\n" +
-                "10: Excluir todos os contatos do vetor\n" +
-                "11: Impreme vetor\n" +
-                "0: sair";
+  public void createContactDynamically(int quantity, Lists<Contact> list) {
+    Contact contact;
 
-        while (!inputValid) {
-            System.out.println(option);
-
-            try {
-                input = scan.nextLine();
-                selectOption = Integer.parseInt(input);
-                if (selectOption >= 0 && selectOption <= 11) {
-                    inputValid = true;
-                } else {
-                    throw new Exception();
-                }
-            } catch (Exception e) {
-                System.out.println("Entrada inválida , digite novamente\n\n");
-            }
-        }
-
-
-        return selectOption;
-    }
-
-    public void createContactDynamically(int quantity, Lists<Contact> list) {
-        Contact contact;
-
-        for (int i = 1; i <= quantity; i++) {
-            contact = new Contact();
-            contact.setName("Contato" + i);
-            contact.setTelephone("111111111" + i);
-            contact.setEmail("Contato" + i + "@gmail.com");
-
-        }
+    for (int i = 1; i <= quantity; i++) {
+      contact = new Contact();
+      contact.setName("Contato" + i);
+      contact.setTelephone("111111111" + i);
+      contact.setEmail("Contato" + i + "@gmail.com");
 
     }
+
+  }
 
 }
--- src/Curse_structure_dates/Vector_base/Lists.java
+++ src/Curse_structure_dates/Vector_base/Lists.java
@@ -4,142 +4,140 @@ import java.lang.reflect.Array;
 
 public class Lists<Type> {
 
-    private Type[] elements;
-    private int size;
+  private Type[] elements;
+  private int size;
 
-    public Lists(int capacity) {
-        this.elements = (Type[]) new Object[capacity];
-        this.size = 0;
+  public Lists(int capacity) {
+    this.elements = (Type[]) new Object[capacity];
+    this.size = 0;
+  }
+
+  public Lists(int capacity, Class<Type> typeClass) {
+    this.elements = (Type[]) Array.newInstance(typeClass, capacity);
+    this.size = 0;
+  }
+
+  public boolean add(Type element) {
+    this.increaseCapacity();
+    if (this.size < this.elements.length) {
+      this.elements[this.size] = element;
+      this.size++;
+      return true;
     }
+    return false;
+  }
 
+  //0 1 2 3 4 5
+  //B C E F G . .
+  public void add(int position, Type element) {
 
-    public Lists(int capacity, Class<Type> typeClass) {
-        this.elements = (Type[]) Array.newInstance(typeClass, capacity);
-        this.size = 0;
+    if (!(position >= 0 && position < size)) {
+      throw new IllegalArgumentException("Posição inválida");
     }
-
-
-    public boolean add(Type element) {
-        this.increaseCapacity();
-        if (this.size < this.elements.length) {
-            this.elements[this.size] = element;
-            this.size++;
-            return true;
-        }
-        return false;
+    this.increaseCapacity();
+    //this.size-1 start in size minus 1
+    for (int i = this.size - 1; i >= position; i--) {
+      this.elements[i + 1] = this.elements[i];
     }
+    this.elements[position] = element;
+    this.size++;
+  }
 
-    //0 1 2 3 4 5
-    //B C E F G . .
-    public void add(int position, Type element) {
-
-        if (!(position >= 0 && position < size)) {
-            throw new IllegalArgumentException("Posição inválida");
-        }
-        this.increaseCapacity();
-        //this.size-1 start in size minus 1
-        for (int i = this.size - 1; i >= position; i--) {
-            this.elements[i + 1] = this.elements[i];
-        }
-        this.elements[position] = element;
-        this.size++;
+  private void increaseCapacity() {
+    if (this.size == this.elements.length) {
+      Type[] newElements = (Type[]) new String[this.elements.length * 2];
+      for (int i = 0; i < this.elements.length; i++) {
+        newElements[i] = this.elements[i];
+      }
+      this.elements = newElements;
     }
+  }
 
-    private void increaseCapacity() {
-        if (this.size == this.elements.length) {
-            Type[] newElements = (Type[]) new String[this.elements.length * 2];
-            for (int i = 0; i < this.elements.length; i++) {
-                newElements[i] = this.elements[i];
-            }
-            this.elements = newElements;
-        }
+
+  //B G D E F -> position will be removed is 1(G)
+  // 0 1 2 3 4 -> size is 6
+  // vector[1] = vector [2]
+  // vector[2] =  vector[3]
+  // vector[3] = vector[4]
+  public void remove(int position) {
+    if (!(position >= 0 && position < size)) {
+      throw new IllegalArgumentException("Posição inválida");
     }
-
-
-    //B G D E F -> position will be removed is 1(G)
-    // 0 1 2 3 4 -> size is 6
-    // vector[1] = vector [2]
-    // vector[2] =  vector[3]
-    // vector[3] = vector[4]
-    public void remove(int position) {
-        if (!(position >= 0 && position < size)) {
-            throw new IllegalArgumentException("Posição inválida");
-        }
-        for (int i = position; i < this.size - 1; i++) {
-            this.elements[i] = this.elements[i + 1];
-        }
-        this.size--;
+    for (int i = position; i < this.size - 1; i++) {
+      this.elements[i] = this.elements[i + 1];
     }
+    this.size--;
+  }
 
-    public void remove(Type element) {
-        int pos = this.search(element);
+  public void remove(Type element) {
+    int pos = this.search(element);
 
-        if (pos > -1) {
-            this.remove(pos);
-        }
+    if (pos > -1) {
+      this.remove(pos);
     }
+  }
 
-    public void clear() {
-        if (this.size > 0) {
-            // this.elements = Type([]) new Object()[this.elements.length];
+  public void clear() {
+    if (this.size > 0) {
+      // this.elements = Type([]) new Object()[this.elements.length];
 
-            this.size = 0;
-            for (int i = 0; i < this.size; i++) {
-                this.elements[i] = null;
-            }
-            this.size = 0;
-        }
+      this.size = 0;
+      for (int i = 0; i < this.size; i++) {
+        this.elements[i] = null;
+      }
+      this.size = 0;
     }
+  }
 
-    public int size() {
-        return this.size;
+  public int size() {
+    return this.size;
+  }
+
+  public Type get(int position) {
+    return this.search(position);
+  }
+
+  public Type search(int position) {
+    if (!(position >= 0 && position < size)) {
+      throw new IllegalArgumentException("Posição inválida");
     }
+    return this.elements[position];
+  }
 
-    public Type get(int position) {
-        return this.search(position);
+  public int search(Type element) {
+    for (int i = 0; i < this.size; i++) {
+      if (this.elements[i].equals(element)) {
+        return i;
+      }
     }
+    return -1;
+  }
 
-    public Type search(int position) {
-        if (!(position >= 0 && position < size)) {
-            throw new IllegalArgumentException("Posição inválida");
-        }
-        return this.elements[position];
+  public int lastIndex(Type element) {
+    for (int i = this.size - 1; i >= 0; i--) {
+      if (this.elements[i].equals(element))
+        return i;
     }
+    return -1;
+  }
 
-    public int search(Type element) {
-        for (int i = 0; i < this.size; i++) {
-            if (this.elements[i].equals(element)) {
-                return i;
-            }
-        }
-        return -1;
+  public boolean contais(Type element) {
+    return search(element) > -1; //>=0
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder text = new StringBuilder();
+    text.append('[');
+    for (int i = 0; i < this.size - 1; i++) {
+      text.append(this.elements[i]);
+      text.append(", ");
     }
-
-    public int lastIndex(Type element) {
-        for (int i = this.size - 1; i >= 0; i--) {
-            if (this.elements[i].equals(element))
-                return i;
-        }
-        return -1;
+    if (this.size > 0) {
+      text.append(this.elements[this.size - 1]);
     }
+    text.append(']');
+    return text.toString();
 
-    public boolean contais(Type element) {
-        return search(element) > -1; //>=0
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder text = new StringBuilder();
-        text.append('[');
-        for (int i = 0; i < this.size - 1; i++) {
-            text.append(this.elements[i]);
-            text.append(", ");
-        }
-        if (this.size > 0) {
-            text.append(this.elements[this.size - 1]);
-        }
-        text.append(']');
-        return text.toString();
-
-    }
+  }
 }
